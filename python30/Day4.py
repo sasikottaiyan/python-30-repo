64c939d87db05e71f7e939d87033525dd3be0ceb
Sasi Kottaiyan S <sasikottaiyan06@gmail.com>
2026-07-07 20:59:40 +0530

    Add files via upload

---
+++ python30/Day4.py
@@ -0,0 +1,42 @@
+# first_name= "sasi"
+# last_name= "kottaiyan"
+# full_name= first_name +" "+ last_name
+# print(full_name)
+
+# print('This is a backslash  symbol (\\)')
+
+
+# name= "sasi"
+# age= 20
+# hobby="painting"
+# sentence= "hello my name is %s, and im %d years old, i love %s"%(name,age,hobby)
+# print(sentence)
+# sentence_2= "hello im {}. im {} years old . i love {}".format(name,age,hobby)
+# print(sentence_2)
+# sentence_3= (f"hello my name is sasi, iam {age} years old")
+# print(sentence_3)
+
+#excersises ===>
+
+# company="Coding for all"
+# print(len(company))
+# print(company.upper())
+# print(company.swapcase())
+# print(company.strip("Coding"))
+# print(company.find("Coding"))
+# print(company.replace("Coding for all","Python"))
+# print(company.split())
+# print(company[0])
+
+
+# a=8
+# b=6
+# print(f'{a+b}')
+# print(" a %d and b %d is",%(a,b))
+
+
+n=int(input("number:"))
+for i in range(0,n):
+    ans=i*i
+    print(ans)
+
---
+++ python30/hackerrank.py
@@ -0,0 +1,4 @@
+n=int(input("number:"))
+for i in range(0,n):
+    ans=i*i
+    print(ans)
