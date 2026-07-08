45915a1b78136d70a6461b31aa9fcff74d1b9cfe
Sasi Kottaiyan S <sasikottaiyan06@gmail.com>
2026-07-08 19:24:03 +0530

    Add files via upload

---
+++ python30/Day6.py
@@ -0,0 +1,41 @@
+#tuple=================================
+
+# tpl=tuple()
+# sisters=('suji','pavi','akshara')
+# brothers=('aravinth','kumar','rajesh')
+# siblings=sisters+brothers
+# print('sibling:', siblings)
+# print('tottal sibs:', len(siblings))
+# family=siblings+('siva','saritha')
+# print(family)
+
+#  f,s,t,fo,fi,*rest=family
+#  print(f,s,t,fo,fi)
+#  print(rest)
+
+# siblings=family[:6]
+# print(siblings)
+# parents=family[6:]
+# print(parents)
+
+
+#level2===============================
+
+# fruits=('apple','banana','mango')
+# veg=('carrot','tomato','potato')
+# animal_prod=('milk','meat')
+# food_stuff_lt= fruits+veg+animal_prod
+# print(food_stuff_lt)
+# midlle=food_stuff_lt[3:5]
+# print(midlle)
+# first_3=food_stuff_lt[0:3]
+# print(first_3)
+# last_3=food_stuff_lt[-3:]
+# print(last_3)
+# del food_stuff_lt
+# print(food_stuff_lt)
+
+
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+check= "Estonia" in nordic_countries, "Iceland" in nordic_countries
+print(check)
