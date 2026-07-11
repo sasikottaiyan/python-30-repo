236b0c72fa610eb9a10cf3254d53c9943c8b0a5b
sasikottaiyan <sasikottaiyan06@gmail.com>
2026-07-11 23:47:16 +0530

    2nd commit

--- Day10.py
+++ Day10.py
@@ -14,7 +14,40 @@
 #     print(count)
 #     count +=1
 
-language = 'Python'
-for i in range(len(language)):
-    print(language[i])
+# language = 'Python'
+# for i in range(len(language)):
+#     print(language[i])
+
+#level 1=============================
+
+# n=10
+# for i in range(n+1):    #for
+#     print(i)
     
+# n=0
+# while n<11:             #while
+#     print(n)
+#     n +=1
+
+# n=0
+# for i in range(10,n-1,-1):  #for
+#     print(i)
+
+# count=10
+# while count>=0:               #while
+#     print(count)
+#     count -=1
+
+
+# count=1
+# while count<=7:                #while
+#     print("#"*count)
+#     count +=1
+    
+n=8
+for i in range(n):
+    for j in range(n):
+        print("#",end="")  
+    print()
+
+
