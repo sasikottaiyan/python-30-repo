065fccd363039db4f76d8424b000e01980e136f7
sasikottaiyan <sasikottaiyan06@gmail.com>
2026-07-11 21:14:52 +0530

    first commit

---
+++ Day10.py
@@ -0,0 +1,20 @@
+#Loops ================
+
+# count = 0
+# while count < 5:
+#     print(count)
+#     count = count + 1
+# else:
+#     print(count)
+
+# count = 0
+# while count < 5:
+#     if count == 3:
+#         count += 1    
+#     print(count)
+#     count +=1
+
+language = 'Python'
+for i in range(len(language)):
+    print(language[i])
+    
