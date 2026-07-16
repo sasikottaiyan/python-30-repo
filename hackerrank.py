7bedb4ba879b20e1ce4d83f072d3c59177fb7515
sasikottaiyan <sasikottaiyan06@gmail.com>
2026-07-16 23:53:38 +0530

    some practice

--- hackerrank.py
+++ hackerrank.py
@@ -1,16 +1,27 @@
-n=int(input("number:"))
-for i in range(0,n):
-    ans=i*i
-    print(ans)
+# n=int(input("number:"))
+# for i in range(0,n):
+#     ans=i*i
+#     print(ans)
 
 
-if __name__ == '__main__':
-    n = int(input().strip())
-if(n%2!=0):
-    print("Weird")
-elif(n%2==0 and n>=2 and n<=5):
-    print("Not Weird")
-elif(n%2==0 and n>=6 and n<=20):
-    print("Wierd")
-elif(n%2==0 and n>20):
-    print("Not Weird")
+# if __name__ == '__main__':
+#     n = int(input().strip())
+# if(n%2!=0):
+#     print("Weird")
+# elif(n%2==0 and n>=2 and n<=5):
+#     print("Not Weird")
+# elif(n%2==0 and n>=6 and n<=20):
+#     print("Wierd")
+# elif(n%2==0 and n>20):
+#     print("Not Weird")
+
+
+#swapping
+a=2
+b=3
+# a,b=b,a
+# print(a,b)
+temp=a
+a=b
+b=temp
+print(a,b)
